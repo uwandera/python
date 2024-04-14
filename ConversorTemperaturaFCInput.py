3b4ba4b9a79ac60456e11f0f73fe6c6812f4efe8
uwandera <tomuwande@gmail.com>
2024-04-14 20:54:20 -0300

    usando a funcionalidade de branch para alocar projetos reinterados e modificados de forma mais dinamica!

--- ConversorTemperaturaFCInput.py
+++ ConversorTemperaturaFCInput.py
@@ -1,5 +1,8 @@
 temperaturaF = float(input("digite uma temperatura em Fahrenheit: "))
 
 temperaturaC = 5*((temperaturaF-32)/9)
+temperaturaK = temperaturaC + 273
 
+print("sua temperatura em fahrenheit é: ", temperaturaF)
 print("a temperetura em celcios é: ",temperaturaC)
+print("sua temperatura em graus Kelvin é: ", temperaturaK)
