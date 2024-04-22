2ba63f60a28ce48f7d4feed1e6217a37b2c82d83
uwandera <tomuwande@gmail.com>
2024-04-21 21:58:54 -0300

    lista de exercicios 1 do curso de Python da USP

---
+++ listadeexercicios1.py
@@ -0,0 +1,6 @@
+lado = int(input("qual o valor do lado do seu quadrado: "))
+
+x = lado*4
+y = x**2
+
+print("perímetor:",x,"área:",y)
---
+++ listadeexercicios2.py
@@ -0,0 +1,8 @@
+n1 = int(input("digite a primeira nota:"))
+n2 = int(input("digite a segunda nota:"))
+n3 = int(input("digite a terceira nota:"))
+n4 = int(input("digite a quarta nota:"))
+
+media = (n1+n2+n3+n4)/4
+
+print("A média aritmética é:",media)
